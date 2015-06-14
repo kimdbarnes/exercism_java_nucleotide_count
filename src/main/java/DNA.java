347d2f538550e16021e3359fd75a91cdbc14a05c
Kim D Barnes <kimdbarnes@gmail.com>
2015-06-13 20:49:04 -0600

    Syntatically correct start

--- src/main/java/DNA.java
+++ src/main/java/DNA.java
@@ -1,2 +1,13 @@
-public DNA {
+public class DNA {
+   public DNA(String s) {
+
+   }
+
+   public Integer count(char c) {
+      return 0;
+   }
+
+   public Object[] nucleotideCounts() {
+      return null;
+   }
 }
