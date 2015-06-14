6f50dfaa61b003a90ab51e454741fbad258fd909
Kim D Barnes <kimdbarnes@gmail.com>
2015-06-13 21:16:31 -0600

    First draft, working

--- src/main/java/DNA.java
+++ src/main/java/DNA.java
@@ -1,13 +1,40 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class DNA {
-   public DNA(String s) {
-
+   public DNA(String dnaString) {
+      this.dnaString = dnaString;
+      this.validNuleotides = "ACTG";
    }
 
-   public Integer count(char c) {
-      return 0;
+   public Integer count(char nucleotide) {
+      if (!isValidNucleotide(nucleotide)) {
+         throw new IllegalArgumentException();
+      }
+
+      int occurrences = 0;
+      for (char character : dnaString.toCharArray()) {
+         if (character == nucleotide) {
+            occurrences++;
+         }
+      }
+      return occurrences;
    }
 
-   public Object[] nucleotideCounts() {
-      return null;
+   public Map<Character, Integer> nucleotideCounts() {
+      Map<Character, Integer> map = new HashMap();
+
+      for (Character nucleotide : validNuleotides.toCharArray()) {
+         map.put(nucleotide, this.count(nucleotide));
+      }
+
+      return map;
+   }
+
+   private String dnaString;
+   private String validNuleotides;
+
+   private boolean isValidNucleotide(char nucleotide) {
+      return validNuleotides.indexOf(nucleotide) >= 0;
    }
 }
